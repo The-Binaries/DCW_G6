41584cf54d9df048368547e6c44b308ee3b9d7fe
Seamoon Pandey <hackedasocial@gmail.com>
2024-08-10 23:00:05 +0545

    chore: Add SafeAreaView and ScrollView to HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,18 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { ScrollView, View, Text, Image, SafeAreaView } from "react-native";
+import { commonStyles } from "../styles/common";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View style={[commonStyles.container, { flex: 1, paddingTop: 48 }]}>
+          <Image
+            source={require("../../assets/icon.png")}
+            style={{ width: 72, height: 48 }}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
---
+++ src/styles/common.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const commonStyles = StyleSheet.create({
+  container: {
+    padding: 16,
+    backgroundColor: "#fff",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
