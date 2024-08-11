85594a06fc18901e2d198d947abb7b123049e819
Seamoon Pandey <hackedasocial@gmail.com>
2024-08-11 23:18:59 +0545

    add badge for the reservation count

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { View, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Image, StyleSheet, TouchableOpacity, Text } from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import commonStyles from "../styles/common";
+import { useSelector } from "react-redux";
+import { selectReservations } from "../features/reservation/reservationSlice";
 
 const TopBar = ({ navigation }) => {
+  const reservations = useSelector(selectReservations);
   const handleAvatarPress = () => {
     navigation.navigate("Reservations");
   };
@@ -20,6 +23,11 @@ const TopBar = ({ navigation }) => {
         style={styles.avatarContainer}
       >
         <Icon name="receipt" size={30} color="black" />
+        {reservations.length > 0 && (
+          <View style={styles.badgeContainer}>
+            <Text style={styles.badgeText}>{reservations.length}</Text>
+          </View>
+        )}
       </TouchableOpacity>
     </View>
   );
@@ -29,13 +37,23 @@ const styles = StyleSheet.create({
   avatarContainer: {
     marginLeft: "auto",
     marginRight: 20,
+    position: "relative",
   },
-  avatar: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    borderWidth: 2,
-    borderColor: "black",
+  badgeContainer: {
+    position: "absolute",
+    top: -5,
+    right: -5,
+    backgroundColor: "red",
+    borderRadius: 10,
+    width: 20,
+    height: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  badgeText: {
+    color: "white",
+    fontSize: 12,
+    fontWeight: "bold",
   },
 });
 
