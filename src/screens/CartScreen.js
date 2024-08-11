9248f3cd7a8bbfb290f90c10d6fda2200e7d1289
Seamoon Pandey <hackedasocial@gmail.com>
2024-08-11 16:18:54 +0545

    reservation add and fix::

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { View, Text, TouchableOpacity, FlatList, Alert } from "react-native";
-import cartStyles from "../styles/cart";
+import { View, Text, TouchableOpacity, FlatList } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 import moment from "moment";
-import { useDispatch } from "react-redux";
-import { addReservation } from "../store/reservationSlice"; // Adjust the import path
+import cartStyles from "../styles/cart";
+import { addReservation } from "../features/reservation/reservationSlice";
+import { clearCart } from "../features/cart/cartSlice";
+import { clearAppointment } from "../features/appointment/appointmentSlice";
 
-const CartScreen = ({ route, navigation }) => {
-  const { selectedDate, selectedTime, selectedServices } = route.params;
+const CartScreen = ({ navigation }) => {
+  const selectedServices = useSelector((state) => state.cart.items);
+  const totalPrice = useSelector((state) => state.cart.totalPrice);
+  const { date: selectedDate, time: selectedTime } = useSelector(
+    (state) => state.appointment
+  );
+
   const dispatch = useDispatch();
-
-  const calculateTotalPrice = () => {
-    return selectedServices.reduce((total, service) => {
-      const price = parseFloat(service.price.replace("$", ""));
-      return total + price;
-    }, 0);
-  };
 
   const handleSubmit = () => {
     if (selectedDate && selectedTime && selectedServices.length > 0) {
@@ -22,11 +22,13 @@ const CartScreen = ({ route, navigation }) => {
         date: selectedDate,
         time: selectedTime,
         services: selectedServices,
-        totalPrice: calculateTotalPrice(),
+        totalPrice: totalPrice,
       };
 
       dispatch(addReservation(reservation));
-      navigation.navigate("ReservationList");
+      navigation.navigate("Home");
+      dispatch(clearCart());
+      dispatch(clearAppointment());
     } else {
       Alert.alert(
         "Incomplete Information",
@@ -45,7 +47,7 @@ const CartScreen = ({ route, navigation }) => {
 
   return (
     <View style={cartStyles.container}>
-      <Text style={cartStyles.header}>Your Cart</Text>
+      <Text style={cartStyles.header}>Confirmation Screen</Text>
       {selectedServices && selectedServices.length > 0 ? (
         <FlatList
           data={selectedServices}
@@ -57,11 +59,15 @@ const CartScreen = ({ route, navigation }) => {
       )}
       <View style={cartStyles.appointmentInfo}>
         <Text>
-          Appointment Date: {moment(selectedDate).format("MMMM D, YYYY")}
+          Appointment Date:{" "}
+          {selectedDate ? moment(selectedDate).format("MMMM D, YYYY") : "N/A"}
         </Text>
-        <Text>Appointment Time: {moment(selectedTime).format("h:mm A")}</Text>
+        <Text>
+          Appointment Time:{" "}
+          {selectedTime ? moment(selectedTime).format("h:mm A") : "N/A"}
+        </Text>
         <Text style={cartStyles.totalPriceText}>
-          Grand Total: ${calculateTotalPrice().toFixed(2)}
+          Grand Total: ${totalPrice.toFixed(2)}
         </Text>
       </View>
       <View style={cartStyles.buttonContainer}>
